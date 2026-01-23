89053a38149472a59f9208dbe0f5cef386321b88
Harsh Vardhan Soni <sonihv01@gmail.com>
2026-01-23 22:07:49 +0530

    get todo

--- app/todo/get-todo/page.tsx
+++ app/todo/get-todo/page.tsx
@@ -1,7 +1,36 @@
 "use client";
 
+import { useEffect, useState } from "react";
+import { toast } from "react-hot-toast";
+
 const TodoPage = () => {
-  return <></>;
+  const [todos, setTodos] = useState("");
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    // Fetch todos here
+    const Todo = async () => {
+      const todo = await fetch("/api/todo/get-todo");
+      if (!todo) {
+        toast.error("errors");
+      }
+      const data = await todo.json();
+      setTodos(data);
+      setLoading(false);
+    };
+    Todo();
+  }, []);
+
+  return (
+    <>
+      {loading ? (
+        <>loading.....</>
+      ) : (
+        <>
+          <>kuch kaam dhanda krle bsdk</>
+        </>
+      )}
+    </>
+  );
 };
 //this he can see the todos created by authenticated user
 export default TodoPage;
